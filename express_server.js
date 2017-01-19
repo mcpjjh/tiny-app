2b611413980e5805c11f20228dab4d9689fba453
Young <mcpjjh@gmail.com>
2017-01-19 23:54:17 +0000

    created registration page and link, created login page and link

--- express_server.js
+++ express_server.js
@@ -16,6 +16,14 @@ const urlDatabase = {
   "P8oWzh": "http://www.theweathernetwork.com"
 };
 
+const userDatabase = {
+    userID1 : {
+      id: "randomID",
+      email: "young@gmail.com",
+      password: "asdf"
+  }
+};
+
 function generateRandomString(length, characs) {
     var result = '';
     for (var i = length; i > 0; --i) {
@@ -24,19 +32,96 @@ function generateRandomString(length, characs) {
     return result;
 }
 
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+function generateRandomID(length, user) {
+    var result = '';
+    for (var i = length; i > 0; --i) {
+    result += user[Math.floor(Math.random() * user.length)];
+   }
+    return result;
+}
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("user_id", req.body.email);
   res.redirect("/urls");
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie("username", req.body.username);
-  res.redirect("/urls");
+app.get("/login", (req, res) => {
+  res.render("urls_login");
 });
+
+app.post("/login", (req, res) => {
+  for (var userEmail in userDatabase) {
+    if (req.body.email === userDatabase[userEmail].email && req.body.password === userDatabase[userEmail].password) {
+      res.cookie("user_id", userDatabase[userEmail].id);
+      res.redirect("/urls");
+    }
+  }
+    res.status(403).send("Access Forbidden")
+    // res.end("Email or password is incorrect.")
+});
+
+// var emailMatch = false;
+// var passwordMatch = false;
+
+//     emailMatch = true
+//   }
+// }
+// if (emailMatch) {
+//   for (var userPassword in userDatabase) {
+//     if (req.body.password === userDatabase[userPassword].id) {
+//       passwordMatch = true
+//     }
+//   }
+// } else if (emailMatch === false) {
+//   res.status(403).send("Access Forbidden")
+// } else {
+//   res.cookie("username", [req.body.id]);
+//     // let user = {
+//     //   id: generateRandomID(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+//     //   email: req.body.email,
+//     //   password: req.body.password
+//     // }
+//   res.redirect("/url");
+// }
+// });
+
+//LOGIN
+
+app.get("/register", (req, res) => {
+  res.render("urls_register");
+});
+
+app.post("/register", (req, res) => {
+
+  var emailExists = false;
+
+  for (var userKey in userDatabase) {
+    if (req.body.email === userDatabase[userKey].email) {
+      emailExists = true
+    }
+  }
+  if (emailExists) {
+    res.status(400).send("ERROR: Email provided is already registered.")
+  } else if (req.body.email === "" || req.body.password === "") {
+    res.status(400).send("ERROR: Email and/or password was left blank.")
+  } else {
+      res.cookie("user_id", [req.body.email]);
+      let user = {
+        id: generateRandomID(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+        email: req.body.email,
+        password: req.body.password
+      }
+  userDatabase[user.id] = user;
+
+  res.redirect("/urls")
+  }
+});
+
+//REGISTER
 
 app.get("/urls", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"],
+    email: req.cookies["user_id"],
     urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
@@ -64,14 +149,14 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);
 });
 
+//NEW
+
 app.post("/urls/:id/update", (req, res) => {
   var shortURL = req.params.id;
   var longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
 });
-
-//NEW
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
